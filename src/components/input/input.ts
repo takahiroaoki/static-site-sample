945343e5d45377f1ef84aac0187ad4be71a227d2
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2026-04-29 06:29:23 +0000

    :sparkles: input component

---
+++ src/components/input/input.ts
@@ -0,0 +1,45 @@
+import { BaseComponent } from "@src/cores/base_component";
+
+export const EVENT_INPUT_VALIDATED = 'event_input_validated';
+
+type InputState = {
+    value: string;
+    validationFunc: (value: string) => Error|null;
+    isValid: boolean;
+};
+
+export class Input extends BaseComponent<InputState> {
+    constructor(elem: HTMLElement, initialState: InputState) {
+        super(elem, initialState);
+    }
+
+    protected getEventSettings() {
+        return [
+            {
+                selector: '.input__content',
+                eventName: 'input',
+                callback: () => {
+                    this.setState({
+                        value: this.getInputValue(),
+                        isValid: false,
+                    })
+                }
+            }
+        ];
+    }
+
+    private getInputValue(): string {
+        const inputElem = this.select('.input__content') as HTMLInputElement;
+        return inputElem.value;
+    }
+
+    protected render() {
+        const { value, validationFunc } = this.getState();
+        const error = validationFunc(value);
+
+        const messageElem = this.select('.input__message') as HTMLElement;
+        messageElem.textContent = error?.message ?? '';
+        this.setState({ isValid: error == null }, { render: false, emit: false });
+        this.emit(EVENT_INPUT_VALIDATED);
+    }
+}
--- src/pages/sample1/index.ts
+++ src/pages/sample1/index.ts
@@ -1,5 +1,21 @@
+import { EVENT_INPUT_VALIDATED, Input } from "@src/components/input/input";
+
 const index = async () => {
-    window.alert('sample1');
+    const input = new Input(document.querySelector('[data-ref="input1-ref"]')!, {
+        value: '',
+        validationFunc: (value: string) => {
+            if (value.length < 5) {
+                return new Error('length must be at least 5 characters');
+            }
+            return null;
+        },
+        isValid: false,
+    });
+    input.on(EVENT_INPUT_VALIDATED, () => {
+        if (input.getState().isValid) {
+            window.alert('Input is valid!');
+        }
+    });
 };
 
 document.addEventListener('DOMContentLoaded', index);
